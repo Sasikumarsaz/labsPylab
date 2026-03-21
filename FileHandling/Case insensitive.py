dc70e1f08c947063c1dc8140cb096e2dd035a5ad
Sasikumarsaz <sasikumar.manimaran4595@gmail.com>
2026-03-21 20:25:15 +0530

    Modify regex and file handling for case insensitive search
    Updated regex word to 'iphone' and removed unused variables. Adjusted file handling to maintain output functionality.

---
+++ FileHandling/Case insensitive.py
@@ -0,0 +1,7 @@
+import re
+word = "iphone"
+
+with open("C:/Users/Sasik/Documents/file1test.txt", "r")
+    for line in inputfile:
+        if re.search(word, line, re.IGNORECASE):    # to ignore the case sensitivity
+            print(line)
