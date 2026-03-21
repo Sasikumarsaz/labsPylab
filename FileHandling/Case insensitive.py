68b44e01496afbedca0724a992d2bfc89f115b2a
Sasikumarsaz <sasikumar.manimaran4595@gmail.com>
2026-03-21 20:31:49 +0530

    Use context manager for file handling

--- FileHandling/Case insensitive.py
+++ FileHandling/Case insensitive.py
@@ -1,7 +1,7 @@
 import re
 word = "iphone"
 
-with open("C:/Users/Sasik/Documents/file1test.txt", "r")
+with open("C:/Users/Sasik/Documents/file1test.txt", "r") as inputfile:
     for line in inputfile:
         if re.search(word, line, re.IGNORECASE):    # to ignore the case sensitivity
             print(line)
