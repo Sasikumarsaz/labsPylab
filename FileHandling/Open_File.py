2eaf0253eb55f5673ee771b2043b93a687dcc6c4
Sasikumarsaz <sasikumar.manimaran4595@gmail.com>
2026-03-21 16:56:58 +0530

    Implement file reading in Open_File.py
    Add file reading functionality to Open_File.py

---
+++ FileHandling/Open_File.py
@@ -0,0 +1,7 @@
+with open("C:/Users/Sasik/Documents/file1test.txt","r") as inputfile:
+  print(inputfile,read())
+
+a = open("C:/Users/Sasik/Documents/file1test.txt","r")
+print(a.read())
+a.close()
+
